da703ea2d580a615c39fc169b0d63814a8737d77
opuzayed <zayedopu4933@gmail.com>
2022-10-29 23:58:20 +0600

    first commit

---
+++ simple-node-server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Simple node server running');
+});
+
+const users = [
+    {id : 1, name : 'sabana', email : 'saba@gmail.com'},
+    {id : 2, name : 'sabila', email : 'sabila@gmail.com'},
+    {id : 3, name : 'kabila', email : 'kabila@gmail.com'}
+];
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
