5601521269c9f304605289ec3f4ef425a3a2c988
opuzayed <zayedopu4933@gmail.com>
2022-10-30 02:12:27 +0600

    data save on server which is send by client side

--- simple-node-client/src/App.js
+++ simple-node-client/src/App.js
@@ -12,14 +12,31 @@ function App() {
 
   const handleAddUser = event => {
     event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+     method : 'POST',
+     headers : {
+      'Content-Type' : 'application/json',
+     },
+     body : JSON.stringify(user), 
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    form.reset();
   }
 
   return (
     <div className="App">
       <form onSubmit={handleAddUser}>
-        <input type="text" name="name" id="" />
+        <input type="text" name="name" id="" placeholder='Enter your name'/>
         <br />
-        <input type="email" name="email" id="" />
+        <input type="email" name="email" id="" placeholder='Enter your email'/>
         <br />
         <button type="submit">Add User</button>
       </form>
--- simple-node-server/index.js
+++ simple-node-server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json()); //req.body te undefined dekhacche tar jonno likte hoi.
 
 app.get('/', (req, res) => {
     res.send('Simple node server running');
@@ -17,7 +18,16 @@ const users = [
 
 app.get('/users', (req, res) => {
     res.send(users);
-})
+});
+
+app.post('/users', (req,res) => {
+    console.log('post api called');
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log(user);
+    res.send(user);
+});
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
