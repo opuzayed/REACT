0b898fbdd7f72dcdd383948675e8c0d0c9b651eb
opuzayed <zayedopu4933@gmail.com>
2022-10-30 00:41:49 +0600

    form make

--- simple-node-client/src/App.js
+++ simple-node-client/src/App.js
@@ -10,8 +10,19 @@ function App() {
     .then(data => setUsers(data));
   },[]);
 
+  const handleAddUser = event => {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
       <h3>Users : {users.length}</h3>
       {
         users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
