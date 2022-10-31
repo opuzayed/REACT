b9644facbec53b171a6e7ed4db665c255c44c25a
opuzayed <zayedopu4933@gmail.com>
2022-10-31 21:14:12 +0600

    post method and delete method

---
+++ node-mongo-crud-server/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+/* user:DataUser2
+pass:QAIPkAQ1PzhVS2MJ */
+
+const uri = "mongodb+srv://DataUser2:QAIPkAQ1PzhVS2MJ@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const userCollection = client.db('CrudMongoDb').collection('users');
+
+        app.get('/users', async (req, res)=> {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.delete('/users/:id', async(req,res) => {
+            const id = req.params.id;
+            console.log('trying to delete ' , id);
+            const query = {_id:ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello from node server');
+});
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+});
