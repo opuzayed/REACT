333246453abd331f68f980dfc0162c7e735a82ae
opuzayed <zayedopu4933@gmail.com>
2022-11-03 01:05:00 +0600

    basic ndoe mongo

---
+++ module-65/genius-car-server/index.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('genius car running');
+})
+
+app.listen(port, () => {
+    console.log(`port on running : ${port}`);
+})
